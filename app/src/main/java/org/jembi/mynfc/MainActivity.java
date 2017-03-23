9b0e18e74c3d54d8923c4f20b3b35c4186d4d761
Unknown <dwyerbaz@gmail.com>
2017-03-23 17:57:51 +0200

    Refactor

--- app/src/main/java/org/jembi/mynfc/MainActivity.java
+++ app/src/main/java/org/jembi/mynfc/MainActivity.java
@@ -1,36 +1,18 @@
 package org.jembi.mynfc;
 
-import android.app.Activity;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.nfc.FormatException;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
-import android.nfc.tech.Ndef;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.apache.commons.lang3.SerializationUtils;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-
 
 public class MainActivity extends AppCompatActivity {
-    public static final String MIME_TEXT_PLAIN = "text/plain";
-
-    public static final String ERROR_DETECTED = "No NFC tag detected!";
-    public static final String WRITE_SUCCESS = "Text written to the NFC tag successfully!";
-    public static final String WRITE_ERROR = "Error during writing, is the NFC tag close enough to your device?";
 
     private TextView nfcOutput;
     private NfcAdapter nfcAdapter;
@@ -39,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     TextView message;
     Tag myTag;
     NfcReader nfcReader;
+    NfcWriter nfcWriter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         context = this;
 
         nfcReader = new NfcReader(nfcOutput);
+        nfcWriter = new NfcWriter(context, myTag);
 
         if (nfcAdapter == null) {
             // Stop here, we definitely need NFC
@@ -70,23 +54,8 @@ public class MainActivity extends AppCompatActivity {
         {
             @Override
             public void onClick(View v) {
-                try {
-                    if(myTag ==null) {
-                        Toast.makeText(context, ERROR_DETECTED, Toast.LENGTH_LONG).show();
-                    } else {
-                        //write(message.getText().toString(), myTag);
-                        write(GetPatient(), myTag);
-                        Toast.makeText(context, WRITE_SUCCESS, Toast.LENGTH_LONG ).show();
-                    }
-                } catch (IOException e) {
-                    Toast.makeText(context, WRITE_ERROR, Toast.LENGTH_LONG ).show();
-                    e.printStackTrace();
-                } catch (FormatException e) {
-                    Toast.makeText(context, WRITE_ERROR, Toast.LENGTH_LONG ).show();
-                    e.printStackTrace();
-                }
+                nfcWriter.tryWrite(GetPatient());
             }
-
         });
 
         nfcReader.handleIntent(getIntent());
@@ -98,39 +67,6 @@ public class MainActivity extends AppCompatActivity {
         patient.Married = false;
         patient.Age = 69;
         return patient;
-    }
-
-    private void write(Patient patient, Tag tag) throws IOException, FormatException {
-        NdefRecord[] records = { createRecord(patient) };
-        NdefMessage message = new NdefMessage(records);
-        // Get an instance of Ndef for the tag.
-        Ndef ndef = Ndef.get(tag);
-        // Enable I/O
-        ndef.connect();
-        // Write the message
-        ndef.writeNdefMessage(message);
-        // Close the connection
-        ndef.close();
-    }
-
-    private NdefRecord createRecord(Patient patient) throws UnsupportedEncodingException {
-        String lang       = "en";
-        byte[] textBytes  = SerializationUtils.serialize(patient);
-        byte[] langBytes  = lang.getBytes("UTF-8");
-        int    langLength = langBytes.length;
-        int    textLength = textBytes.length;
-        byte[] payload    = new byte[1 + langLength + textLength];
-
-        // set status byte (see NDEF spec for actual bits)
-        payload[0] = (byte) langLength;
-
-        // copy langbytes and textbytes into payload
-        System.arraycopy(langBytes, 0, payload, 1,              langLength);
-        System.arraycopy(textBytes, 0, payload, 1 + langLength, textLength);
-
-        NdefRecord recordNFC = new NdefRecord(NdefRecord.TNF_WELL_KNOWN,  NdefRecord.RTD_TEXT,  new byte[0], payload);
-
-        return recordNFC;
     }
 
     @Override
